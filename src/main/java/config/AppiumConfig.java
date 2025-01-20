3393525d7a29c0520b144e5b060a459cc4c47f46
Alina-QA-Engineer <alinaqatests@gmail.com>
2025-01-20 18:56:02 +0200

    Add AppiumConfig

---
+++ src/main/java/config/AppiumConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumConfig {
+
+    public static AppiumDriver<AndroidElement> driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "Nexus 5.1");
+        desiredCapabilities.setCapability("platformVersion", "9.0");
+        desiredCapabilities.setCapability("appPackage", "com.telran.ilcarro");
+        desiredCapabilities.setCapability("appActivity", "SplashActivity");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+
+        String urlNexus5_1 = "http://localhost:4723/wd/hub";
+
+        try {
+            driver = new AppiumDriver<>(new URL(urlNexus5_1), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        //driver.quit();
+    }
+
+}
